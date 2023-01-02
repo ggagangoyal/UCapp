e2af3ea7d4518cb0003bb9ea9c4ad4d85b6241e5
gagangoyal <goyalgagan82@gmail.com>
2023-01-02 23:09:37 +0530

    add one to many association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
+ has_many :articles
 	validates :username, presence: true, 
                      uniqueness: {case_sensitive: false}, 
                      length: { minimum: 3, maximum: 25}
- VALID_EMAIL_REGEX = /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
- validates :email, presence: true,  
+ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+                     validates :email, presence: true,  
                      uniqueness: {case_sensitive: false}, 
                      length: {maximum: 105},
                      format: {with: VALID_EMAIL_REGEX}
 end
+
