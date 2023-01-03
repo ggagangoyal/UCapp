645cb345bb2d98654263ba0b2c9c963ea0d5867d
gagangoyal <goyalgagan82@gmail.com>
2023-01-03 20:06:48 +0530

    add logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
   resources :users,  except: [:new]
   resources :articles   #, only: [:show, :index, :new, :create,:edit,:update,:destroy]
 end
